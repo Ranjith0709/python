0fb048999af5f9765baa2727fee0be0034850ac8
medipallyranjith <89833085+medipallyranjith@users.noreply.github.com>
2021-09-02 14:41:11 +0530

    Add files via upload

---
+++ WEEK2.py
@@ -0,0 +1,49 @@
+a=eval(input("enter the a value"))
+b=eval(input("enter the b value"))
+c=a+b
+print(c)
+
+
+
+n=eval(input("enter the numder between 1-7:"))
+if n==1:
+  print("monday")
+elif n==2:
+   print("tuesday")
+elif n==3:
+   print("wednesday")
+elif n==4:
+   print("thursday")
+elif n==5:
+   print("friday")
+elif n==6:
+   print("saturday")
+elif n==7:
+   print("sunday")
+else:
+     print("please enter number between 1-7:")
+
+
+a=eval(input("enter the a value:"))
+b=eval(input("enter the b value:"))
+c=eval(input("enter the c value:"))
+if a>b:
+  if a>c:
+   print("a is greatest value")
+  else:
+   print("c is greatest value")
+else:
+  if b>c:
+    print("b is greatest value") 
+  else:
+    print("c is greatest value")
+
+
+
+n=eval(input("enter the value:"))
+if n>0:
+  print("postive numbers")
+else:
+    print("negative number")
+
+    
